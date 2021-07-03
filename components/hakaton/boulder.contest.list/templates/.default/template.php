281f6aa1e37594eee4260569dd83432b8c923838
Tania <rnnchik@gmail.com>
2021-07-03 15:14:03 +0200

    Список соревнований

--- components/hakaton/boulder.contest.list/templates/.default/template.php
+++ components/hakaton/boulder.contest.list/templates/.default/template.php
@@ -3,27 +3,75 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 /**
  * @var array $arResult
  */
-?><h1>Contest list</h1>
-<ul><?php
-	$date = new \Bitrix\Main\Type\Date();
-foreach ($arResult['ITEMS'] as $item)
-{
-	?><li>
-		<dl>
-			<dt><?=($item['NAME'] ?? 'Contest')?></dt>
-			<dd><?=$item['START_DATE']?>-<?=$item['FINISH_DATE']?></dd>
-		</dl>
-		<?php
-		if ($item['START_DATE'] <= $date && $date <= $item['FINISH_DATE'])
-		{
-			?><br/>
-			<a href="?page=card&contest_id=<?=$item['ID']?>">Participate!</a><?php
-		}
-		?>
-	</li><?php
-}
-?></ul>
+?>
 
-<p><a href="?page=apply">Apply!</a></p>
-<p><a href="?page=card">Go to the card</a></p>
-<p><a href="?page=result">Go to results</a></p>
+<!doctype html>
+<html lang="ru">
+<head>
+<!--	<meta charset="UTF-8">-->
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Список соревнований</title>
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="<?=$templateFolder?>/style.css">
+</head>
+<body>
+<main>
+
+	<h1 class="title">Contest list</h1>
+
+	<div class="table">
+		<dib class="table-row table-row-block">
+			<div class="name table-cell">Янтарные вершины</div>
+			<div class="flex table-cell date">
+				<div class="start">03.07.2021</div>
+				<div class="end">04.07.2021</div>
+			</div>
+			<div class=" table-cell opportunities">
+				<a href="#" class="btn btn-save btn-take-part">Принять участие</a>
+			</div>
+		</dib>
+
+		<dib class="table-row table-row-block">
+			<div class="name table-cell">Текущее  соревнование</div>
+			<div class="flex table-cell date">
+				<div class="start">02.06.2021</div>
+				<div class="end">04.06.2021</div>
+			</div>
+			<div class=" table-cell opportunities">
+				<a href="#" class="btn btn-save btn-take-part">Принять участие</a>
+			</div>
+		</dib>
+
+		<dib class="table-row table-row-block">
+			<div class="name table-cell">Янтарные горы</div>
+			<div class="flex table-cell date">
+				<div class="start">03.03.2021</div>
+				<div class="end">04.03.2021</div>
+			</div>
+			<div class=" table-cell opportunities">
+				<a href="#" class="btn btn-results">Посмотреть результаты</a>
+			</div>
+		</dib>
+
+	</div>
+
+
+
+
+<!--	<p><a href="?page=apply">Apply!</a></p>-->
+<!--	<p><a href="?page=card">Go to the card</a></p>-->
+<!--	<p><a href="?page=result">Go to results</a></p>-->
+</main>
+
+</body>
+</html>
+
+
+
+
+
